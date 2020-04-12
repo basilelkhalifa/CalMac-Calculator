22b29c48da7d5672854c33896bc05fa5e78b9ed6
basilelkhalifa <basilosama.2000@gmail.com>
2020-04-12 23:57:58 +0300

    added transtion effects

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  useLocation,
+} from "react-router-dom";
 import "./App.css";
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
@@ -7,6 +12,7 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import CaloriesContainer from "./components/CaloriesContainer";
 import ResultsContainer from "./components/ResultsContainer";
+import { AnimatePresence } from "framer-motion";
 
 function App() {
   const [userData, setUserData] = useState();
@@ -46,21 +52,27 @@ function App() {
         redirectPath={redirectValue}
         scrolled={scrolled}
       />
-      <Route
-        exact
-        path="/"
-        render={(props) => <Home {...props} onClick={handleClickHome} />}
-      />
-      <Route
-        path="/calories"
-        render={(props) => (
-          <CaloriesContainer {...props} onClick={handleClick} />
-        )}
-      />
-      <Route
-        path="/results"
-        render={(props) => <ResultsContainer {...props} userInput={userData} />}
-      />
+      <AnimatePresence exitBeforeEnter>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={(props) => <Home {...props} onClick={handleClickHome} />}
+          />
+          <Route
+            path="/calories"
+            render={(props) => (
+              <CaloriesContainer {...props} onClick={handleClick} />
+            )}
+          />
+          <Route
+            path="/results"
+            render={(props) => (
+              <ResultsContainer {...props} userInput={userData} />
+            )}
+          />
+        </Switch>
+      </AnimatePresence>
 
       <About />
       <Contact />
