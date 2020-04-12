b585a6eb0aa29ba9ab410be0e843b69ebc3e995b
basilelkhalifa <basilosama.2000@gmail.com>
2020-04-12 23:53:26 +0300

    fixed alignment issue

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ import { Container, Button, Row, Col } from "react-bootstrap";
 export default function About() {
   return (
     <Container className="about font-weight-light">
-      <Row className="my-5 center-text">
+      <Row className="my-5 center-text mx-auto">
         <Col sm={12} lg={6}>
           <Container fluid>
             <h1 className="font-weight-light pb-3">About CalMac</h1>
@@ -30,7 +30,7 @@ export default function About() {
         </Col>
       </Row>
 
-      <Row className="my-5">
+      <Row className="my-5  mx-auto">
         <Col sm={12} lg={6} className="order-2 order-lg-1">
           <img
             src="https://images.unsplash.com/photo-1498837167922-ddd27525d352?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
@@ -56,7 +56,7 @@ export default function About() {
       </Row>
 
       <Row>
-        <Container className="text-center my-3">
+        <Container className="text-center my-3  mx-auto">
           <h3 className="font-weight-light learn-more">
             Learn more about calories and macronutrients!
           </h3>
